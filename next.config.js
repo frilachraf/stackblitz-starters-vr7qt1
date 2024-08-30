7e1388864c3e65795fec7af2c9e3d78b4a5ed83d
frilachraf <frilachraf.dev@gmail.com>
2024-08-30 17:21:02 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/stackblitz-starters-vr7qt1",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
